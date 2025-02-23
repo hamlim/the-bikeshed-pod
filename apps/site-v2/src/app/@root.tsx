10647c9255fcae729601b77b360b1db9d4e07b77
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-02-23 09:07:35 -0500

    WIP banner

--- apps/site-v2/src/app/@root.tsx
+++ apps/site-v2/src/app/@root.tsx
@@ -15,6 +15,16 @@ function themeCheck() {
   });
 }
 
+function WIPBanner() {
+  return (
+    <div className="bg-[repeating-linear-gradient(-45deg,#f7df1e_0,#f7df1e_10px,#000_10px,#000_20px)] py-2 flex justify-center items-center">
+      <p className=" text-white bg-black text-center font-bold px-4">
+        ⚠️ This site is a work in progress! ⚠️
+      </p>
+    </div>
+  );
+}
+
 export default function Root({ children }: { children: ReactNode }) {
   // @TODO: Make sure this preload is used below in the body!
   // geist-regular / geist sans isn't used currently
@@ -34,6 +44,7 @@ export default function Root({ children }: { children: ReactNode }) {
           // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
           dangerouslySetInnerHTML={{ __html: `(${themeCheck.toString()})()` }}
         />
+        <WIPBanner />
         <Nav />
         {children}
       </body>

c1e8d73047e36babcc90a8ec645dd1ed289c13b4
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-03-26 09:43:35 -0400

    Add episode 3

---
+++ apps/site-v2/src/app/episodes/3/a-day-in-the-life-coding-coffee-and-commit-messages/page.tsx
@@ -0,0 +1,10 @@
+import { EpisodeContainer } from "#components/episode-container";
+import Content, { frontmatter as rawMetadata } from "./episode-3.mdx";
+
+export default function Page() {
+  return (
+    <EpisodeContainer rawMetadata={rawMetadata}>
+      <Content />
+    </EpisodeContainer>
+  );
+}

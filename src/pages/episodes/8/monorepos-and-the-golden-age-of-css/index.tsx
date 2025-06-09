ca5e76687a3c7e3752e4ff0032853419b5d15b3d
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-06-09 17:12:41 -0400

    Add episode 8

---
+++ src/pages/episodes/8/monorepos-and-the-golden-age-of-css/index.tsx
@@ -0,0 +1,18 @@
+import { EpisodeContainer } from "#components/episode-container";
+import Content, { frontmatter as rawMetadata } from "./episode-8.mdx";
+
+export default function Page() {
+  return (
+    <EpisodeContainer rawMetadata={rawMetadata}>
+      <Content />
+    </EpisodeContainer>
+  );
+}
+
+
+export function getConfig() {
+  return {
+    render: "dynamic",
+  };
+}
+

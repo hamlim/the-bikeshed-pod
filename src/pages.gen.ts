e478949353bf30ed0e6a83cc85458c042da115cc
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-11-11 16:51:13 -0500

    Episode 14

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -14,6 +14,8 @@ import type { getConfig as File_Episodes12IsTheWebScrewedIndex_getConfig } from 
 // prettier-ignore
 import type { getConfig as File_Episodes13TheDownfallOfReactIndex_getConfig } from './pages/episodes/13/the-downfall-of-react/index';
 // prettier-ignore
+import type { getConfig as File_Episodes14TheParkingLotIndex_getConfig } from './pages/episodes/14/the-parking-lot/index';
+// prettier-ignore
 import type { getConfig as File_Episodes2IsTheWebGettingWorseIndex_getConfig } from './pages/episodes/2/is-the-web-getting-worse/index';
 // prettier-ignore
 import type { getConfig as File_Episodes3ADayInTheLifeCodingCoffeeAndCommitMessagesIndex_getConfig } from './pages/episodes/3/a-day-in-the-life-coding-coffee-and-commit-messages/index';
@@ -39,6 +41,7 @@ type Page =
 | ({ path: '/episodes/11/deploy-fast-and-break-things' } & GetConfigResponse<typeof File_Episodes11DeployFastAndBreakThingsIndex_getConfig>)
 | ({ path: '/episodes/12/is-the-web-screwed' } & GetConfigResponse<typeof File_Episodes12IsTheWebScrewedIndex_getConfig>)
 | ({ path: '/episodes/13/the-downfall-of-react' } & GetConfigResponse<typeof File_Episodes13TheDownfallOfReactIndex_getConfig>)
+| ({ path: '/episodes/14/the-parking-lot' } & GetConfigResponse<typeof File_Episodes14TheParkingLotIndex_getConfig>)
 | ({ path: '/episodes/2/is-the-web-getting-worse' } & GetConfigResponse<typeof File_Episodes2IsTheWebGettingWorseIndex_getConfig>)
 | ({ path: '/episodes/3/a-day-in-the-life-coding-coffee-and-commit-messages' } & GetConfigResponse<typeof File_Episodes3ADayInTheLifeCodingCoffeeAndCommitMessagesIndex_getConfig>)
 | ({ path: '/episodes/4/perfecting-the-pull-request' } & GetConfigResponse<typeof File_Episodes4PerfectingThePullRequestIndex_getConfig>)

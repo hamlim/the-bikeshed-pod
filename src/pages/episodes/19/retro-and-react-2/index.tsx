2e0bd4ec4d62f9007216f8025768beae79e20f14
Matt Hamlin <matthewjameshamlin@gmail.com>
2026-01-18 20:32:04 -0500

    Episode 19

---
+++ src/pages/episodes/19/retro-and-react-2/index.tsx
@@ -0,0 +1,16 @@
+import { EpisodeContainer } from "#components/episode-container";
+import Content, { frontmatter as rawMetadata } from "./episode-19.mdx";
+
+export default function Page() {
+  return (
+    <EpisodeContainer rawMetadata={rawMetadata}>
+      <Content />
+    </EpisodeContainer>
+  );
+}
+
+export function getConfig() {
+  return {
+    render: "dynamic",
+  };
+}
